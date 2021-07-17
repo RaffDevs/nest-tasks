a473c5fd028a43ec37c687b0ea2a9977b1335d89
RaffDevs <rveronezdeveloper@gmail.com>
2021-07-17 18:20:38 -0300

    Implementação dos controllers e services do modulo Tasks

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateTaskDTO } from './dto/create-task.dto';
+import { Task } from './tasks.model';
+import { TasksService } from './tasks.service';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  constructor(private taskService: TasksService) {
+    this.taskService = taskService;
+  }
+
+  @Get('')
+  async getAllTasks(): Promise<Task[]> {
+    return this.taskService.getAllTasks();
+  }
+
+  @Post('')
+  async createTask(@Body() taskDTO: CreateTaskDTO): Promise<Task> {
+    return this.taskService.createTask(taskDTO);
+  }
+
+}
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { CreateTaskDTO } from './dto/create-task.dto';
+import { Task, TaskStatus } from './tasks.model';
+import { v4 as uuid } from 'uuid';
+
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  private tasks: Task[] = [];
+
+  public getAllTasks(): Task[] {
+    return this.tasks;
+  }
+
+  public createTask(taskToSave: CreateTaskDTO): Task {
+    const { title, description } = taskToSave;
+    
+    let task: Task = {
+      id: uuid(),
+      title: title,
+      description: description,
+      status: TaskStatus.DONE
+    }
+
+    this.tasks.push(task);
+
+    return task;
+  }
+}
